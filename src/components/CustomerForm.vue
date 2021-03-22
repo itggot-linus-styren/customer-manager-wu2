df1d664f750b5dd34c83443471ca4d29ec0a58b4
Linus Styrén <linus.styren@ga.ntig.se>
2021-03-22 21:31:53 +0100

    lägg till pure-group

--- src/components/CustomerForm.vue
+++ src/components/CustomerForm.vue
@@ -1,20 +1,24 @@
 <template>
   <form data-cy="form" class="pure-form">
-    <fieldset>
+    <fieldset class="pure-group">
+      <legend>Kundinformation</legend>
       <input
         type="text"
+        class="pure-input-1"
         placeholder="Förnamn"
         :value="customer.firstname"
         :readonly="!editable"
       />
       <input
         type="text"
+        class="pure-input-1"
         placeholder="Efternamn"
         :value="customer.lastname"
         :readonly="!editable"
       />
       <input
         type="email"
+        class="pure-input-1"
         placeholder="E-mail"
         :value="customer.email"
         :readonly="!editable"
@@ -45,5 +49,10 @@ export default {
 <style scoped>
 input[readonly] {
   color: white;
+}
+
+form {
+  max-width: 400px;
+  margin: 0 auto;
 }
 </style>
