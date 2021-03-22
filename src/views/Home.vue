d9b25e1968d169d59913c753d09ccbf4de9da947
Linus Styrén <linus.styren@ga.ntig.se>
2021-03-22 12:13:03 +0100

    skapa Home sidan

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Kunder</h1>
+    <hr />
+    <table>
+      <thead>
+        <th>Förnamn</th>
+        <th>Efternamn</th>
+        <th>E-mail</th>
+        <th></th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Namn</td>
+          <td>Namnsson</td>
+          <td>namn.namnsson@gmail.com</td>
+          <td><button>Visa</button></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
