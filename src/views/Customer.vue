bc8fc3e86d73ace4fb96579dced4cf101bfd4a12
Linus Styrén <linus.styren@ga.ntig.se>
2021-03-22 22:51:27 +0100

    lägg till uppgiftsbeskrivning

--- src/views/Customer.vue
+++ src/views/Customer.vue
@@ -1,4 +1,11 @@
 <template>
+  <router-link
+    data-cy="edit"
+    :to="`/customers/${$route.params.id}/edit`"
+    class="pure-button button-secondary"
+    >Redigera</router-link
+  >
+  <button data-cy="del" class="pure-button button-danger">Ta bort</button>
   <CustomerForm :initialCustomer="customer" />
 </template>
 
@@ -23,8 +30,20 @@ export default {
     console.log("Customer.vue@created: " + this.$route.params.id);
     const customers = JSON.parse(localStorage.getItem("customers"));
     this.customer = customers[this.$route.params.id];
-  }
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.pure-button {
+  margin: 0.5em;
+}
+
+.button-danger {
+  background: rgb(202, 60, 60);
+}
+
+.button-secondary {
+  background: rgb(66, 184, 221);
+}
+</style>
